3e94146e784004a8235d8367ccc8160efcb24ef3
benjaminvickers <benjamin.vickers@yahoo.co.uk>
2016-01-21 16:09:38 +0000

    Initial commit. Work in progress

---
+++ main.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var dbConfig = require('./configs/db');
+var Employee = require('./models/employee');
+
+
+mongoose.connect(dbConfig.url);
+
+// Close the database connection when Node process ends.
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    process.exit(0);
+  });
+});
+
+
+var promise = new Promise(function(resolve, reject) {
+  
+  // Clear the local.Employee collection.
+  Employee.remove({}, function(err) {
+    if(err) {
+      reject(err);
+      return;
+    }
+    
+    resolve();
+  });
+})
+.then(function() {
+
+  // Persist new Employee document.  
+  return new Promise(function(resolve, reject) {
+
+    var date1 = new Date();
+    var date2 = new Date();
+    date2.setDate(date1.getDate() + 1);
+
+    var flexitime1 = {"date":date1, "duration":"60", "note":"Started work one hour early on 1/1/2016"};
+    var flexitime2 = {"date":date2, "duration":"30", "note":"Started work half an hour early on 2/1/2016"};
+
+    var employee = new Employee();
+    employee.email = 'root@localhost';
+    employee.firstname = 'Benjamin';
+    employee.lastname = 'Vickers';
+    employee.flexitimeaccrued.push(flexitime1);
+    employee.flexitimeaccrued.push(flexitime2);
+    employee.isactive = true;
+    
+    employee.save(function(err) {
+      if(err) {
+        reject(err);
+        return;
+      }
+    
+      console.log('Document created successfully.');
+      resolve(employee._id);
+    });
+  });
+})
+.then(function(employeeId) {
+
+  // Update the new Employee document.
+  return new Promise(function(resolve, reject) {
+  
+    Employee.findById(employeeId, function(err, employee) {
+      if(err) {
+        reject(err);
+        return;
+      }
+      
+      // Insert another embedded document.
+      var date = new Date();
+      date.setDate(date.getDate() + 2);
+      
+      var flexitime3 = {"date":date, "duration":"120", "note":"Early start on 3/1/2016"};
+      
+      employee.flexitimeaccrued.push(flexitime3);
+      employee.save(function(err) {
+        if(err) {
+          reject(err);
+          return;
+        }
+        
+        console.log('Document updated successfully.');
+        resolve(employee);
+      });
+    });
+  });
+})
+.then(function(employee) {
+
+  // HERE: Find by other means, find by embedded collections etc
+  console.log('Finalising..');
+  process.exit(0);
+})
+.catch(function(err) {
+
+  console.log('An error occured: ' + err);
+});
+
+
+
+
+
---
+++ models/employee.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+// The FlexitimeSchema will be embedded within the EmployeeSchema.
+var FlexitimeSchema = new Schema({
+
+  // _id will be created by default
+  date: Date,
+  duration: Number,
+  note: String
+});
+
+var EmployeeSchema = new Schema({
+
+  // _id will be created by default
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  firstname: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  lastname: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  flexitimeaccrued: [FlexitimeSchema],
+  flexitimeused: [FlexitimeSchema],
+  balance: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  isactive: {
+    type: Boolean,
+    required: true,
+    default: true
+  }
+});
+
+
+// HERE Attach methods to the EmployeeSchema
+// https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+
+// Generate a Model from the Schema.
+var Employee = mongoose.model('Employee', EmployeeSchema);
+
+module.exports = Employee;
